3cea513f2583e3e9a3acbe034e928e53fc7b99c6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-12 11:34:31 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("inserisci un carattere");
+            s = Console.ReadLine();
+            foreach (char c in s)
+            {
+                Console.WriteLine((int)c);
+                Console.ReadLine();
+            }
+        }
+    }
+}
